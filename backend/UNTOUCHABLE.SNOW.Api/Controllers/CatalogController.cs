1d03ba64996017d1d00847cbd51ed352f4138321
shaus0909 <shauryas09@gmail.com>
2021-04-08 17:16:15 -0400

    commit

---
+++ backend/UNTOUCHABLE.SNOW.Api/Controllers/CatalogController.cs
@@ -0,0 +1,61 @@
+---------------------------------------
+Create catalog controller inside the API project by adding CatalogController.cs to
+the Controllers folder, with the following code:
+
+using Microsoft.AspNetCore.Mvc;
+using Untouchable.Snow.Domain;
+
+namespace Untouchable.Snow.Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CatalogController : ControllerBase
+	{
+	[HttpGet]
+
+		public IActionResult GetItems()
+			{
+			var items = new List<Item>()
+    		{
+        		new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+        		new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+    		};
+
+    return Ok(items);
+			}
+	[HttpGet("{id:int}")]
+		public IActionResult GetItem(int id)
+		{
+    		var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    		item.Id = id;
+
+    		return Ok(item);
+		}
+		[HttpPost]
+		public IActionResult Post(item item)
+		{
+    		return Created("/catalog/42", item);
+		}
+		[HttpPost("{id:int}/ratings")]
+		public IActionResult PostRating(int id, [FromBody] Rating rating)
+		{
+    		var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    		item.Id = id;
+    		item.AddRating(rating);
+
+    		return Ok(item);
+		}
+		[HttpPost("{id:int}/ratings")]
+		public IActionResult PutItem(int id, [FromBody] Item item)
+		{
+   			 return Ok();
+		}
+		[HttpDelete]
+		public IActionResult DeleteItem(int id)
+		{
+    		return Ok();
+		}
+
+
+	}
+}
